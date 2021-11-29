21fc2b127b4981d21106db146cf0b7a13fc4ee39
wendellunny <wendelferreiraportugal@gmail.com>
2021-11-29 06:28:41 -0300

    Limpando código e fazendo abstrações

--- app/Models/CityHall.php
+++ app/Models/CityHall.php
@@ -1,27 +1,14 @@
 <?php
 
 namespace App\Models;
-
-use App\Traits\CityHallTrait;
-use Carbon\Carbon;
+use App\Traits\IBGE;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Http;
-
-define(
-    'CITY_API_URL',
-    'https://servicodados.ibge.gov.br/api/v1/localidades/municipios/'
-);
-
-define(
-    'CITY_POPULATION_API_URL',
-    'https://servicodados.ibge.gov.br/api/v3/agregados/6579/periodos/{$year}/variaveis/9324?localidades=N6[{$id_city}]'
-);
 
 class CityHall extends Model
 {
     use HasFactory;
-    use CityHallTrait;
+    use IBGE;
     
     protected $fillable = [
         'social_reason',
--- app/Traits/IBGE.php
+++ app/Traits/IBGE.php
@@ -5,12 +5,24 @@ namespace App\Traits;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
 
-trait CityHallTrait{
+define(
+    'CITY_API_URL',
+    'https://servicodados.ibge.gov.br/api/v1/localidades/municipios/'
+);
+
+define(
+    'CITY_POPULATION_API_URL',
+    'https://servicodados.ibge.gov.br/api/v3/agregados/6579/periodos/{$year}/variaveis/9324?localidades=N6[{$id_city}]'
+);
+
+trait IBGE{
     
     protected static function getDataCity($id_city){
         $data = Http::get( CITY_API_URL . $id_city );
+
         $dataCity =  self::cleanDataCityApi($data->object());
         $dataCity['population']  = self::getCityPopulation($id_city);
+
         return $dataCity;
     }
 
@@ -25,12 +37,13 @@ trait CityHallTrait{
     protected static function getCityPopulation($id_city){
         $apiUrl = self::getPopulationApiUrlWithVariables($id_city);
         $population = Http::get($apiUrl);
+
         return self::cleanDataPopulation($population->object());
     }
 
     protected static function getPopulationApiUrlWithVariables($id_city){
-        $apiUrl = str_replace('{$id_city}' , $id_city , CITY_POPULATION_API_URL);
         $year = Carbon::now()->format('Y');
+        $apiUrl = str_replace('{$id_city}' , $id_city , CITY_POPULATION_API_URL);
         $apiUrl = str_replace('{$year}',$year,$apiUrl);
         return $apiUrl;
     }
